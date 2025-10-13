2c1f1c2c1a16d31f93ec17c110eff5a3b121a453
lgwk42 <luisgunkim@gmail.com>
2025-10-13 16:19:24 +0900

    feat: register job api add

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/job/client/api/JobApiHandler.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/job/client/api/JobApiHandler.java
@@ -1,9 +1,14 @@
 package com.bigtablet.bigtablethompageserver.domain.job.client.api;
 
 import com.bigtablet.bigtablethompageserver.domain.job.application.usecase.JobUseCase;
+import com.bigtablet.bigtablethompageserver.domain.job.client.dto.request.RegisterJobRequest;
+import com.bigtablet.bigtablethompageserver.global.common.dto.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
@@ -13,5 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobApiHandler {
 
     public final JobUseCase jobUseCase;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BaseResponse registerJob(RegisterJobRequest request) {
+        jobUseCase.registerJob(request);
+        return BaseResponse.created("등록 성공");
+    }
 
 }
