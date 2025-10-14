533dc42f9c8032ed1430e7c324491d5de016253a
lgwk42 <luisgunkim@gmail.com>
2025-10-14 15:29:30 +0900

    fix: register recruit request

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/recruit/client/dto/request/RegisterRecruitRequest.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/recruit/client/dto/request/RegisterRecruitRequest.java
@@ -2,8 +2,11 @@ package com.bigtablet.bigtablethompageserver.domain.recruit.client.dto.request;
 
 import com.bigtablet.bigtablethompageserver.domain.recruit.domain.enums.EducationLevel;
 import com.bigtablet.bigtablethompageserver.domain.recruit.domain.enums.Military;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
 
 public record RegisterRecruitRequest(
         @NotNull
@@ -13,23 +16,37 @@ public record RegisterRecruitRequest(
         @NotBlank
         String phoneNumber,
         @NotBlank
+        @Email(message = "이메일 형식이 틀립니다.")
         String email,
         @NotBlank
         String address,
         @NotBlank
         String addressDetail,
         @NotBlank
+        @URL(message = "유효한 URL 형식이어야 합니다.")
         String portfolio,
+        @URL(message = "유효한 URL 형식이어야 합니다.")
         String coverLetter,
         @NotNull
         EducationLevel educationLevel,
         String schoolName,
+        @Pattern(
+                regexp = "^(19|20)\\d{2}-(0[1-9]|1[0-2])$",
+                message = "입학년도는 YYYY-MM 형식이어야 합니다."
+        )
         String admissionYear,
+        @Pattern(
+                regexp = "^(19|20)\\d{2}-(0[1-9]|1[0-2])$",
+                message = "졸업년도는 YYYY-MM 형식이어야 합니다."
+        )
         String graduationYear,
         String department,
         @NotNull
         Military military,
+        @URL(message = "유효한 URL 형식이어야 합니다.")
         String attachment1,
+        @URL(message = "유효한 URL 형식이어야 합니다.")
         String attachment2,
+        @URL(message = "유효한 URL 형식이어야 합니다.")
         String attachment3
 ) {}
