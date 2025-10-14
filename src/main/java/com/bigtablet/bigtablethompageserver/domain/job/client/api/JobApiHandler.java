43baec36016fcc2f493309a3b504db26dcd3e07c
lgwk42 <luisgunkim@gmail.com>
2025-10-14 11:03:38 +0900

    delete: swagger annotation

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/job/client/api/JobApiHandler.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/job/client/api/JobApiHandler.java
@@ -3,8 +3,6 @@ package com.bigtablet.bigtablethompageserver.domain.job.client.api;
 import com.bigtablet.bigtablethompageserver.domain.job.application.usecase.JobUseCase;
 import com.bigtablet.bigtablethompageserver.domain.job.client.dto.request.RegisterJobRequest;
 import com.bigtablet.bigtablethompageserver.global.common.dto.response.BaseResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -17,14 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/job")
-@Tag(name = "채용공고", description = "채용공고 API")
 public class JobApiHandler {
 
     public final JobUseCase jobUseCase;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "채용공고 등록", description = "채용공고를 등록합니다")
     public BaseResponse registerJob(RegisterJobRequest request) {
         jobUseCase.registerJob(request);
         return BaseResponse.created("등록 성공");
--- src/main/java/com/bigtablet/bigtablethompageserver/domain/user/client/api/UserApiHandler.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/user/client/api/UserApiHandler.java
@@ -3,8 +3,6 @@ package com.bigtablet.bigtablethompageserver.domain.user.client.api;
 import com.bigtablet.bigtablethompageserver.domain.user.application.response.UserResponse;
 import com.bigtablet.bigtablethompageserver.domain.user.application.service.UserService;
 import com.bigtablet.bigtablethompageserver.global.common.dto.response.BaseResponseData;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
-@Tag(name = "유저", description = "유저 API")
 public class UserApiHandler {
 
     public final UserService userService;
@@ -27,7 +24,6 @@ public class UserApiHandler {
      *
      * */
     @GetMapping
-    @Operation(summary = "유저 정보 토큰 기반 조회", description = "토큰을 기반으로 유저 정보를 조회합니다")
     public BaseResponseData<UserResponse> getUserByToken() {
         return BaseResponseData.ok(
                 "조회 성공",
