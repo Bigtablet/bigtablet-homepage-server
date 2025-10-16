a4fe51325750ca4c346509e405e125bfe55dd47b
lgwk42 <luisgunkim@gmail.com>
2025-10-16 15:45:59 +0900

    bug: add generated value in recruit entity idx

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/recruit/domain/entity/RecruitEntity.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/recruit/domain/entity/RecruitEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -25,6 +27,7 @@ public class RecruitEntity extends BaseEntity {
 
     @Id
     @Column(nullable = false, unique = true, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
     @Column(nullable = false)
