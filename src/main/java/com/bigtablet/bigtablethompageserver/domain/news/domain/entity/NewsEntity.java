512d578ae58b55cdcead7b7baca9715f69fc507f
lgwk42 <luisgunkim@gmail.com>
2025-10-16 13:04:19 +0900

    fix: table name

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/news/domain/entity/NewsEntity.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/news/domain/entity/NewsEntity.java
@@ -16,7 +16,7 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Getter
 @SuperBuilder
-@Table(name = "tb_job")
+@Table(name = "tb_news")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NewsEntity extends BaseEntity {
 
