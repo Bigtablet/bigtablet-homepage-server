add1b465f310d917a623d029010ca7a805ea4089
lgwk42 <luisgunkim@gmail.com>
2025-12-05 14:02:05 +0900

    fix: add validation in register blog request

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/blog/client/dto/request/RegisterBlogRequest.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/blog/client/dto/request/RegisterBlogRequest.java
@@ -1,9 +1,16 @@
 package com.bigtablet.bigtablethompageserver.domain.blog.client.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record RegisterBlogRequest(
+        @NotBlank
         String titleKr,
+        @NotBlank
         String titleEn,
+        @NotBlank
         String contentKr,
+        @NotBlank
         String contentEn,
+        @NotBlank
         String imageUrl
 ) {}
