298494fc9f10270df7a299c1e51302b78eabfa10
lgwk42 <luisgunkim@gmail.com>
2025-12-05 14:19:39 +0900

    fix: add talent already exist error/exception

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/talent/exception/error/TalentError.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/talent/exception/error/TalentError.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum TalentError implements ErrorProperty {
 
     TALENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 인재를 찾을 수 없습니다."),
+    TALENT_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 등록된 인재 입니다"),
     TALENT_IS_EMPTY(HttpStatus.NO_CONTENT, "등록된 인재가 없습니다.");
 
     private final HttpStatus status;
