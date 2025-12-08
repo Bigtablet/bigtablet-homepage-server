2ca49cf832df83967d48bfe922d93aaa085c1e9c
lgwk42 <luisgunkim@gmail.com>
2025-12-08 11:00:04 +0900

    feat: add get talent api

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/talent/client/api/TalentApiHandler.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/talent/client/api/TalentApiHandler.java
@@ -1,16 +1,21 @@
 package com.bigtablet.bigtablethompageserver.domain.talent.client.api;
 
+import com.bigtablet.bigtablethompageserver.domain.talent.application.response.TalentResponse;
 import com.bigtablet.bigtablethompageserver.domain.talent.application.usecase.TalentUseCase;
 import com.bigtablet.bigtablethompageserver.domain.talent.client.dto.request.RegisterTalentRequest;
 import com.bigtablet.bigtablethompageserver.domain.talent.client.dto.request.SendEmailToTalentRequest;
 import com.bigtablet.bigtablethompageserver.global.common.annotation.RestApiHandler;
 import com.bigtablet.bigtablethompageserver.global.common.dto.response.BaseResponse;
+import com.bigtablet.bigtablethompageserver.global.common.dto.response.BaseResponseData;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Validated
@@ -32,6 +37,12 @@ public class TalentApiHandler {
     public BaseResponse sendMailToTalent(@RequestBody @Valid final SendEmailToTalentRequest request) {
         talentUseCase.sendMailToTalent(request);
         return BaseResponse.ok("전송 성공");
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponseData<TalentResponse> getTalent(@RequestParam @NotNull final Long idx) {
+        return BaseResponseData.ok("조회 성공", talentUseCase.getTalent(idx));
     }
 
 }
