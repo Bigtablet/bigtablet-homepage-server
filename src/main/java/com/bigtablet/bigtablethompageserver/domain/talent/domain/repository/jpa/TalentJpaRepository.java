a26a6c21c7533f2a34794e2fd208d0b5936adb92
luis <luisgunkim@gmail.com>
2026-03-31 10:05:51 +0900

    fix: remove unnecessary pessimistic lock from TalentJpaRepository

--- src/main/java/com/bigtablet/bigtablethompageserver/domain/talent/domain/repository/jpa/TalentJpaRepository.java
+++ src/main/java/com/bigtablet/bigtablethompageserver/domain/talent/domain/repository/jpa/TalentJpaRepository.java
@@ -1,11 +1,7 @@
 package com.bigtablet.bigtablethompageserver.domain.talent.domain.repository.jpa;
 
 import com.bigtablet.bigtablethompageserver.domain.talent.domain.entity.TalentEntity;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,8 +9,6 @@ public interface TalentJpaRepository extends JpaRepository<TalentEntity, Long> {
 
     boolean existsByEmail(String email);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select t from TalentEntity t where t.idx = :idx")
-    Optional<TalentEntity> findByIdx(@Param("idx") Long idx);
+    Optional<TalentEntity> findByIdx(Long idx);
 
 }
